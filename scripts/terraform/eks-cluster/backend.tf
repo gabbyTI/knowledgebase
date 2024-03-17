dc37863603122b590a35c55372d3de2d3dffe914
Gabriel Ibenye <gabrielibenye@gmail.com>
2024-03-17 01:18:48 -0400

    added kubenetes cluster

--- scripts/terraform/eks-cluster/backend.tf
+++ scripts/terraform/eks-cluster/backend.tf
@@ -3,6 +3,6 @@ terraform {
     resource_group_name  = "rg-terraform-backend"
     storage_account_name = "ibenyestorageaccount"
     container_name       = "backendcontainer"
-    key                  = "knowledgebase/jenkins-server/terraform.tfstate"
+    key                  = "knowledgebase/eks/terraform.tfstate"
   }
 }
